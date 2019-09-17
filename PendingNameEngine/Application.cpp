5e8ef72455a289f903b125e5e770e4e76c6fb52f
GuillemD <gdrdvilla@gmail.com>
2019-09-17 14:12:07 +0200

    Added base files

---
+++ PendingNameEngine/Application.cpp
@@ -0,0 +1,109 @@
+#include "Application.h"
+
+Application::Application()
+{
+	window = new ModuleWindow(this);
+	input = new ModuleInput(this);
+	renderer3D = new ModuleRenderer3D(this);
+	camera = new ModuleCamera3D(this);
+
+	// The order of calls is very important!
+	// Modules will Init() Start() and Update in this order
+	// They will CleanUp() in reverse order
+
+	// Main Modules
+	AddModule(window);
+	AddModule(camera);
+	AddModule(input);
+	
+
+	// Renderer last!
+	AddModule(renderer3D);
+}
+
+Application::~Application()
+{
+	for (std::list<Module*>::iterator item = list_modules.begin(); item != list_modules.end(); item++)
+	{
+		delete (*item);
+	}
+
+}
+
+bool Application::Init()
+{
+	bool ret = true;
+
+	// Call Init() in all modules
+	for (std::list<Module*>::iterator item = list_modules.begin(); item != list_modules.end(); item++)
+	{
+		ret = (*item)->Init();
+	}
+
+	// After all Init calls we call Start() in all modules
+	LOG("Application Start --------------");
+	for (std::list<Module*>::iterator item = list_modules.begin(); item != list_modules.end(); item++)
+	{
+		ret = (*item)->Start();
+	}
+	
+	ms_timer.Start();
+	return ret;
+}
+
+// ---------------------------------------------
+void Application::PrepareUpdate()
+{
+	dt = (float)ms_timer.Read() / 1000.0f;
+	ms_timer.Start();
+}
+
+// ---------------------------------------------
+void Application::FinishUpdate()
+{
+}
+
+// Call PreUpdate, Update and PostUpdate on all modules
+update_status Application::Update()
+{
+	update_status ret = UPDATE_CONTINUE;
+	PrepareUpdate();
+	
+	for (std::list<Module*>::iterator item = list_modules.begin(); item != list_modules.end(); item++)
+	{
+		if (ret == UPDATE_CONTINUE)
+			ret = (*item)->PreUpdate(dt);
+	}
+
+	for (std::list<Module*>::iterator item = list_modules.begin(); item != list_modules.end(); item++)
+	{
+		if (ret == UPDATE_CONTINUE)
+			ret = (*item)->Update(dt);
+	}
+
+	for (std::list<Module*>::iterator item = list_modules.begin(); item != list_modules.end(); item++)
+	{
+		if (ret == UPDATE_CONTINUE)
+			ret = (*item)->PostUpdate(dt);
+	}
+
+	FinishUpdate();
+	return ret;
+}
+
+bool Application::CleanUp()
+{
+	bool ret = true;
+
+	for (std::list<Module*>::reverse_iterator item = list_modules.rbegin(); item != list_modules.rend(); item++)
+	{
+		ret = (*item)->CleanUp();
+	}
+
+	return ret;
+}
+
+void Application::AddModule(Module* mod)
+{
+	list_modules.push_back(mod);
+}
---
+++ PendingNameEngine/ModuleCamera3D.h
@@ -0,0 +1,35 @@
+#ifndef _MODULECAMERA_H_
+#define _MODULECAMERA_H_
+
+#include "Module.h"
+#include "Globals.h"
+#include "glmath.h"
+
+
+class ModuleCamera3D : public Module
+{
+public:
+	ModuleCamera3D(Application* app, bool start_enabled = true);
+	~ModuleCamera3D();
+
+	bool Start();
+	update_status Update(float dt);
+	bool CleanUp();
+
+	void Look(const vec3 &Position, const vec3 &Reference, bool RotateAroundReference = false);
+	void LookAt(const vec3 &Spot);
+	void Move(const vec3 &Movement);
+	float* GetViewMatrix();
+
+private:
+	float maxDistanceToVehicle = 25.0f;
+	void CalculateViewMatrix();
+	mat4x4 ViewMatrix, ViewMatrixInverse;
+	bool isPlaying = false;
+public:
+	
+	vec3 X, Y, Z, Position, Reference;
+	
+};
+
+#endif //_MODULECAMERA_H_
