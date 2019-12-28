0877676a221301497e6c4e53e5eb395495ef86a2
GuillemD <gdrdvilla@gmail.com>
2019-12-29 00:04:10 +0100

    Added volume rtcp in wwise

--- PendingNameEngine/WWISE/AK/Wwise_IDs.h
+++ PendingNameEngine/WWISE/AK/Wwise_IDs.h
@@ -32,6 +32,7 @@ namespace AK
         static const AkUniqueID SS_AIR_STORM = 3715662592U;
         static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
         static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+        static const AkUniqueID VOLUME = 2415836739U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
